76427fa034197170c940c3166affb017b840cff0
vuong-viet-hung <vuongviethung156@gmail.com>
2023-09-23 12:45:52 +0700

    Declare train function

--- training/training.py
+++ training/training.py
@@ -5,5 +5,9 @@ def train(
     model: torch.nn.Module,
     train_dl: torch.utils.data.Dataloader,
     val_dl: torch.utils.data.Dataloader,
+    loss: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    scheduler,
+    epochs: int,
 ) -> None:
     pass
