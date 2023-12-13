081b85dd5d56898dea6c503404b920d878eb6816
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-13 22:38:30 +0700

    Create CWRUPipeline class

--- bearing/data/cwru.py
+++ bearing/data/cwru.py
@@ -5,81 +5,70 @@ import re
 import urllib.request
 import zipfile
 from pathlib import Path
+from typing import Self
 
 import numpy as np
 import pandas as pd
 import scipy
-import torchvision
 from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import Dataset
 
-from bearing.data.common import DataFile, DataPipeline, Transform, register_data_pipeline
-
-
-def make_transform(image_size: tuple[int, int] = (64, 64)) -> Transform:
-    return torchvision.transforms.Compose(
-        [
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Resize(image_size, antialias=True),
-        ]
-    )
+from bearing.data.common import DataPipeline, Loader, register_data_pipeline
 
 
 def load_signal(data_file: str | Path) -> np.ndarray:
     data = scipy.io.loadmat(str(data_file))
-    *_, signal_key = (key for key in data.keys() if key.endswith('DE_time'))
-    return data[signal_key].squeeze()
+    *_, signal_key = (key for key in data.keys() if key.endswith("DE_time"))
+    return data[signal_key].astype(np.float32).squeeze()
 
 
-@register_data_pipeline('cwru')
+@register_data_pipeline("cwru")
 class CWRUPipeline(DataPipeline):
+    def download_data(self) -> Self:
+        if self.data_dir.exists():
+            logging.info(f"CWRU dataset has already been downloaded to '{self.data_dir}'.")
+            return self
 
-    def download(self, data_dir: str | Path) -> None:
-        if data_dir.exists():
-            return
+        logging.info(f"Downloading CWRU dataset to '{self.data_dir}'...")
+        download_url = "https://github.com/XiongMeijing/CWRU-1/archive/refs/heads/master.zip"
+        download_zip_file = Path("CWRU-1-master.zip")
+        download_extract_dir = Path("CWRU-1-master")
+        urllib.request.urlretrieve(download_url, download_zip_file)
 
-        repo_url = 'https://github.com/XiongMeijing/CWRU-1/archive/refs/heads/master.zip'
-        repo_zip_file = Path('CWRU-1-master.zip')
-        repo_dir = Path('CWRU-1-master')
-        logging.info(f'Downloading \'cwru\' dataset to {data_dir!r}...')
-        urllib.request.urlretrieve(repo_url, repo_zip_file)
-
-        with zipfile.ZipFile(repo_zip_file, 'r') as zip_ref:
+        with zipfile.ZipFile(download_zip_file, "r") as zip_ref:
             zip_ref.extractall()
 
-        repo_zip_file.unlink()
-        (repo_dir / 'Data').rename(data_dir)
-        shutil.rmtree(repo_dir)
+        download_zip_file.unlink()
+        (download_extract_dir / "Data").rename(self.data_dir)
+        shutil.rmtree(download_extract_dir)
+        return self
 
-    def make_df(self, data_dir: str | Path) -> pd.DataFrame:
-        data_dir = Path(data_dir)
-        normal_data_files = (data_dir / 'Normal').glob('*.mat')
-        fault_data_files = (data_dir / '12k_DE').glob('*.mat')
+    def get_data_frame(self) -> pd.DataFrame:
+        normal_data_files = (self.data_dir / "Normal").glob("*.mat")
+        fault_data_files = (self.data_dir / "12k_DE").glob("*.mat")
         data_files = itertools.chain(normal_data_files, fault_data_files)
-        df = pd.DataFrame(data_files, columns=['file'])
+        df = pd.DataFrame(data_files, columns=["file"])
         file_regex = re.compile(
-            r'''
+            r"""
             ([a-zA-Z]+)  # Fault
             (\d{3})?  # Fault size
             (@\d+)?  # Fault location
             _
             (\d+)  # Load
             \.mat
-            ''',
+            """,
             re.VERBOSE,
         )
-        df['sampling_rate'] = 12_000
-        df['match'] = df.file.map(lambda file: file_regex.match(file.name))
-        df['fault'] = df.match.map(lambda match: match.group(1))
-        df['fault_size'] = df.match.map(lambda match: match.group(2))
-        df['fault_location'] = df.match.map(lambda match: match.group(3))
-        df['load'] = df.match.map(lambda match: match.group(4))
-        df.drop(columns=['match'], inplace=True)
-        df['rpm'] = df.load.map({'0': 1797, '1': 1772, '2': 1750, '3': 1730})
+        df["sample_rate"] = 12_000
+        df["match"] = df.file.map(lambda file: file_regex.match(file.name))
+        df["fault"] = df.match.map(lambda match: match.group(1))
+        df["fault_size"] = df.match.map(lambda match: match.group(2))
+        df["fault_location"] = df.match.map(lambda match: match.group(3))
+        df["load"] = df.match.map(lambda match: match.group(4))
+        df.drop(columns=["match"], inplace=True)
+        df["rpm"] = df.load.map({"0": 1797, "1": 1772, "2": 1750, "3": 1730})
         encoder = LabelEncoder()
-        df['label'] = encoder.fit_transform(df.fault)
+        df["label"] = encoder.fit_transform(df.fault)
         return df
 
-    def make_data_file(self, data_row: pd.Series, segment_len: int, num_samples: int | None) -> Dataset:
-        transform = make_transform()
-        return DataFile(data_row, segment_len, load_signal, transform, num_samples)
+    def get_loader(self) -> Loader:
+        return load_signal
