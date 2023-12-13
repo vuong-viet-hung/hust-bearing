34f2cc939dca00059a4428bb063965124922069a
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-13 15:09:22 +0700

    Add batch_size parameter to get_data_loader method

--- bearing/data/common.py
+++ bearing/data/common.py
@@ -8,13 +8,14 @@ from bearing.data.cwru import CWRUPipeline
 
 
 class DataPipeline(ABC):
-    def get_data_loaders(self, data_dir: Path | str) -> tuple[DataLoader, DataLoader, DataLoader]:
+
+    def get_data_loaders(self, data_dir: Path | str, batch_size: int) -> tuple[DataLoader, DataLoader, DataLoader]:
         self.download_data(data_dir)
         data_frame = self.get_data_frame(data_dir)
         train_df, valid_df, test_df = self.split_data_frame(data_frame)
-        train_dl = self.get_data_loader(train_df)
-        valid_dl = self.get_data_loader(valid_df)
-        test_dl = self.get_data_loader(test_df)
+        train_dl = self.get_data_loader(train_df, batch_size)
+        valid_dl = self.get_data_loader(valid_df, batch_size)
+        test_dl = self.get_data_loader(test_df, batch_size)
         return train_dl, valid_dl, test_dl
 
     @abstractmethod
@@ -28,7 +29,7 @@ class DataPipeline(ABC):
     def split_data_frame(self, data_frame: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         pass
 
-    def get_data_loader(self, data_frame: pd.DataFrame) -> DataLoader:
+    def get_data_loader(self, data_frame: pd.DataFrame, batch_size: int) -> DataLoader:
         pass
 
 
