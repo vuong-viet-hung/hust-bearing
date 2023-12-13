56561f6ad3fa688964fc8439d6cc7a30e0794821
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-14 04:05:39 +0700

    Remove train_load argument

--- bearing/models/train.py
+++ bearing/models/train.py
@@ -9,7 +9,6 @@ def main() -> None:
     parser = ArgumentParser()
     parser.add_argument("--data-dir", type=Path)
     parser.add_argument("--dataset-name", type=str)
-    parser.add_argument("--train-load", type=str)
     parser.add_argument("--batch-size", type=int)
     parser.add_argument("--logging-level", type=str, default="info")
     parser.add_argument("--seed", type=int, default=21)
@@ -17,11 +16,10 @@ def main() -> None:
 
     data_dir: Path | None = args.data_dir
     dataset_name: str = args.dataset_name
-    train_load: str = args.train_load
     batch_size: int = args.batch_size
-    logging_level: str = args.logging_level.upper()
+    logging_level: str = args.logging_level
 
-    logging.basicConfig(level=getattr(logging, logging_level), format="%(message)s")
+    logging.basicConfig(level=getattr(logging, logging_level.upper()), format="%(message)s")
 
     if data_dir is None:
         data_root_dir = Path("data")
@@ -32,7 +30,7 @@ def main() -> None:
     (
         data_pipeline
         .download_data()
-        .build_datasets(train_load)
+        .build_datasets()
         .build_data_loaders(batch_size)
         .validate_data_loaders()
     )
