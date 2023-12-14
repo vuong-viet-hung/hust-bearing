164e8b6e98198fefe26fef67f77e1648edbd4274
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-14 15:29:36 +0700

    Rename package to hust_bearing

--- bearing/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from bearing.data.common import DataPipeline, get_data_pipeline
-from bearing.data import cwru
---
+++ hust_bearing/data/__init__.py
@@ -0,0 +1,2 @@
+from hust_bearing.data.common import DataPipeline, get_data_pipeline
+from hust_bearing.data import cwru
