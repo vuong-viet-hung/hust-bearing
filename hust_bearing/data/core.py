b4d1475fb8c9d4760cd3b1a67972553e021e9d51
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-15 23:41:26 +0700

    Add data truncation

--- hust_bearing/data/core.py
+++ hust_bearing/data/core.py
@@ -229,7 +229,7 @@ class Pipeline(ABC):
         )
         self.subsets[subset] = TransformDataset(self.subsets[subset], transform)
         self.build_data_loader(subset)
-        logging.debug(f"{subset}: range=({-outlier:.2f}, {outlier:.2f})")
+        logging.debug(f"{subset}: mean={pixel_mean}, std={pixel_std})")
 
     def min_max_scale(self, subset: Subset) -> None:
         data_loader = self.data_loaders[subset]
