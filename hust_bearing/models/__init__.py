29c57c044fd270e4dab2f37ebb5d25362fa053b6
vuong-viet-hung <vuongviethung156@gmail.com>
2023-12-15 21:24:52 +0700

    Add VGG16 model

--- hust_bearing/models/__init__.py
+++ hust_bearing/models/__init__.py
@@ -1,2 +1,3 @@
 from hust_bearing.models.core import Engine, build_model
 from hust_bearing.models.lenet5 import LeNet5
+from hust_bearing.models.vgg16 import VGG16
---
+++ hust_bearing/models/vgg16.py
@@ -0,0 +1,43 @@
+from torch import nn
+
+from hust_bearing.models.core import register_model
+
+
+@register_model("vgg16")
+class VGG16(nn.Module):
+    def __init__(self, num_classes: int) -> None:
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 8 * 8, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.9),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.9),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
