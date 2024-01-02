8032b6b3cbc4981a3afaef8ad99f038ea5e5469f
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-02 10:13:22 +0700

    Update data module

--- hust_bearing/data/__init__.py
+++ hust_bearing/data/__init__.py
@@ -1,3 +1,1 @@
-from hust_bearing.data.core import Pipeline, build_pipeline
-from hust_bearing.data.cwru import CWRUPipeline
-from hust_bearing.data.hust import HUSTBearingPipeline
+from hust_bearing.data.cwru import CWRU
