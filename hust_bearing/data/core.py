754991d41cb1ede542ae1c982f3c7b7038929f99
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-07 13:09:56 +0700

    Renamed from common.py

---
+++ hust_bearing/data/core.py
@@ -0,0 +1,141 @@
+from abc import ABCMeta, abstractmethod
+from itertools import chain
+from pathlib import Path
+
+import lightning as pl
+import joblib
+import numpy as np
+import scipy
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import DataLoader
+
+
+class SpectrogramDM(pl.LightningDataModule, metaclass=ABCMeta):
+    def __init__(self, train_load: str, data_dir: Path | str, batch_size: int) -> None:
+        super().__init__()
+        self.train_load = train_load
+        self.data_dir = Path(data_dir)
+        self.batch_size = batch_size
+
+    def prepare_data(self) -> None:
+        if not self.data_dir.exists():
+            self.download()
+        self._init_paths()
+        self._init_labels()
+
+    def setup(self, stage: str) -> None:
+        if stage == "fit":
+            self._train_ds = Spectrograms(self._train_paths, self._train_labels)
+            self._val_ds = Spectrograms(self._val_paths, self._val_labels)
+
+        elif stage == "validate":
+            self._val_ds = Spectrograms(self._val_paths, self._val_labels)
+
+        else:
+            self._test_ds = Spectrograms(self.test_paths, self._test_labels)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self._train_ds, self.batch_size, num_workers=8, shuffle=True)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self._test_ds, self.batch_size, num_workers=8)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self._val_ds, self.batch_size, num_workers=8)
+
+    def predict_dataloader(self) -> DataLoader:
+        return self.test_dataloader()
+
+    @abstractmethod
+    def download(self) -> None:
+        pass
+
+    @abstractmethod
+    def extract_label(self, dir_name: str) -> str:
+        pass
+
+    @abstractmethod
+    def extract_load(self, dir_name: str) -> str:
+        pass
+
+    def _init_paths(self) -> None:
+        paths = list(self.data_dir.glob("**/*.mat"))
+
+        fit_paths = [
+            path
+            for path in paths
+            if self.extract_load(path.parent.name) == self.train_load
+        ]
+        self._test_paths = [
+            path
+            for path in paths
+            if self.extract_load(path.parent.name) != self.train_load
+        ]
+
+        labels = [self.extract_label(path.parent.name) for path in fit_paths]
+        self._train_paths, self._val_paths = train_test_split(
+            fit_paths, test_size=0.2, stratify=labels
+        )
+
+    def _init_labels(self) -> None:
+        encoder_path = self.data_dir / "label_encoder.joblib"
+        encoder = _load_encoder(encoder_path, self._get_train_labels())
+
+        self._train_labels = encoder.transform(self._get_train_labels())
+        self._test_labels = encoder.transform(self._get_test_labels())
+        self._val_labels = encoder.transform(self._get_val_labels())
+
+    def _get_train_labels(self) -> list[str]:
+        return [self.extract_label(path.parent.name) for path in self._train_paths]
+
+    def _get_test_labels(self) -> list[str]:
+        return [self.extract_label(path.parent.name) for path in self.test_paths]
+
+    def _get_val_labels(self) -> list[str]:
+        return [self.extract_label(path.parent.name) for path in self._val_paths]
+
+
+class Spectrograms(Dataset):
+    def __init__(
+        self,
+        paths: list[Path],
+        labels: np.ndarray,
+    ) -> None:
+        self._paths = paths
+        self._labels = torch.from_numpy(labels)
+        self._transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Resize((64, 64), antialias=None),
+            ]
+        )
+
+    def __len__(self) -> int:
+        return len(self._paths)
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        spectrogram = _load_spectrogram(self._paths[idx])
+        image = self._transform(spectrogram)
+        return image, self._labels[idx]
+
+
+def _list_dirs(dirs: list[Path]) -> list[Path]:
+    return list(chain.from_iterable(dir_.glob("*.mat") for dir_ in dirs))
+
+
+def _load_encoder(encoder_path: Path, fit_labels: list[str]) -> LabelEncoder:
+    if encoder_path.exists():
+        return joblib.load(encoder_path)
+    encoder = LabelEncoder()
+    encoder.fit(fit_labels)
+    joblib.dump(encoder, encoder_path)
+    return encoder
+
+
+def _load_spectrogram(path: Path) -> torch.Tensor:
+    data = scipy.io.loadmat(str(path))
+    return data["spec"].astype(np.float32)
