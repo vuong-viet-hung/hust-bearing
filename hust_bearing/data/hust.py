2563f9b59c5d1524c402a3b6e4d4210b0c52078b
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-07 13:10:14 +0700

    Create hust.py

--- hust_bearing/data/hust.py
+++ hust_bearing/data/hust.py
@@ -1,51 +1,10 @@
 import re
 from pathlib import Path
 
-from hust_bearing.data.common import MeasuredSpectrogramDM, SimulatedSpectrogramDM
+from hust_bearing.data.core import SpectrogramDM
 
 
-class HUST(MeasuredSpectrogramDM):
-    def __init__(
-        self,
-        train_load: str,
-        data_dir: Path | str = Path("spectrograms", "hust"),
-        batch_size: int = 32,
-    ) -> None:
-        super().__init__(train_load, data_dir, batch_size)
-        self._downloader = HUSTDownloader()
-        self._parser = HUSTDirNameParser()
-
-    def download(self) -> None:
-        return self._downloader.download(self.data_dir)
-
-    def extract_label(self, dir_name: str) -> str:
-        return self._parser.extract_label(dir_name)
-
-    def extract_load(self, dir_name: str) -> str:
-        return self._parser.extract_load(dir_name)
-
-
-class HUSTSim(SimulatedSpectrogramDM):
-    def __init__(
-        self, data_dir: Path | str = Path("spectrograms", "hust"), batch_size: int = 32
-    ) -> None:
-        super().__init__(data_dir, batch_size)
-        self._downloader = HUSTDownloader()
-        self._parser = HUSTDirNameParser()
-
-    def download(self) -> None:
-        return self._downloader.download(self.data_dir)
-
-    def extract_label(self, dir_name: str) -> str:
-        return self._parser.extract_label(dir_name)
-
-
-class HUSTDownloader:
-    def download(self, data_dir: Path | str) -> None:
-        pass
-
-
-class HUSTDirNameParser:
+class HUST(SpectrogramDM):
     _regex = re.compile(
         r"""
         ([a-zA-Z]+)  # Fault
@@ -55,6 +14,17 @@ class HUSTDirNameParser:
         """,
         re.VERBOSE,
     )
+
+    def __init__(
+        self,
+        train_load: str,
+        data_dir: Path | str = Path("spectrograms", "hust"),
+        batch_size: int = 32,
+    ) -> None:
+        super().__init__(train_load, data_dir, batch_size)
+
+    def download(self) -> None:
+        pass
 
     def extract_label(self, dir_name: str) -> str:
         return self._parse(dir_name).group(1)
