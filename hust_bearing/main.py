d51a312d80083deb439f774f8ec87d8fb5412239
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-12 12:37:09 +0700

    Initial commit

--- hust_bearing/main.py
+++ hust_bearing/main.py
@@ -4,9 +4,9 @@ from hust_bearing.models import classifier
 from hust_bearing.data import bearing_data_module
 
 
-def main():
+def cli_main():
     LightningCLI(classifier, bearing_data_module)
 
 
 if __name__ == "__main__":
-    main()
+    cli_main()
