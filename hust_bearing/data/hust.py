a41c33ebc480794796b9675b9ec571ec7f533caa
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-29 15:34:33 +0700

    Fix cyclic import

--- hust_bearing/data/hust.py
+++ hust_bearing/data/hust.py
@@ -1,7 +1,7 @@
 import re
 from pathlib import Path
 
-from hust_bearing.data import BearingDataModule
+from hust_bearing.data.data_module import BearingDataModule
 
 
 class HUST(BearingDataModule):
