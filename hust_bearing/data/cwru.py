61a65bb31605438ed7e9c211345c8d26b6069866
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-29 20:33:16 +0700

    Update cwru.py

--- hust_bearing/data/cwru.py
+++ hust_bearing/data/cwru.py
@@ -1,5 +1,4 @@
 import re
-from pathlib import Path
 
 from hust_bearing.data import BearingDataModule
 
@@ -17,11 +16,11 @@ class CWRU(BearingDataModule):
     )
     _classes = ["Normal", "B", "IR", "OR"]
 
-    def target_from(self, path: Path) -> int:
-        return self._classes.index(self._parse(path.parent.name).group(1))
+    def target_from(self, dir_name: str) -> int:
+        return self._classes.index(self._parse(dir_name).group(1))
 
-    def load_from(self, path: Path) -> int:
-        return int(self._parse(path.parent.name).group(4))
+    def load_from(self, dir_name: str) -> int:
+        return int(self._parse(dir_name).group(4))
 
     def _parse(self, dir_name: str) -> re.Match[str]:
         match = self._dir_name_regex.fullmatch(dir_name)
