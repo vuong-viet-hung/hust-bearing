313a7162356bfc11bf2c9876321fa86a5a536d5c
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-29 20:32:55 +0700

    Update classifier.py

--- hust_bearing/models/classifier.py
+++ hust_bearing/models/classifier.py
@@ -15,31 +15,29 @@ class Classifier(pl.LightningModule, metaclass=ABCMeta):
         super().__init__()
         self.model = model
         self.loss = nn.CrossEntropyLoss()
-        self.train_acc = MulticlassAccuracy(num_classes)
-        self.test_acc = MulticlassAccuracy(num_classes)
-        self.val_acc = MulticlassAccuracy(num_classes)
+        self.accuracy = MulticlassAccuracy(num_classes)
 
     def training_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
         (inputs, targets), *_ = args
         outputs = self.model(inputs)
         loss = self.loss(outputs, targets)
-        self.train_acc(outputs, targets)
-        self.log_dict({"train_loss": loss, "train_acc": self.train_acc}, prog_bar=True)
+        self.accuracy(outputs, targets)
+        self.log_dict({"train_loss": loss, "train_acc": self.accuracy}, prog_bar=True)
         return loss
 
     def validation_step(self, *args: Any, **kwargs: Any) -> None:
         (inputs, targets), *_ = args
         outputs = self.model(inputs)
         loss = self.loss(outputs, targets)
-        self.val_acc(outputs, targets)
-        self.log_dict({"val_loss": loss, "val_acc": self.val_acc}, prog_bar=True)
+        self.accuracy(outputs, targets)
+        self.log_dict({"val_loss": loss, "val_acc": self.accuracy}, prog_bar=True)
 
     def test_step(self, *args: Any, **kwargs: Any) -> None:
         (inputs, targets), *_ = args
         outputs = self.model(inputs)
         loss = self.loss(outputs, targets)
-        self.test_acc(outputs, targets)
-        self.log_dict({"test_loss": loss, "test_acc": self.test_acc}, prog_bar=True)
+        self.accuracy(outputs, targets)
+        self.log_dict({"test_loss": loss, "test_acc": self.accuracy}, prog_bar=True)
 
     def predict_step(self, *args: Any, **kwargs: Any) -> torch.Tensor:
         (inputs, _), *_ = args
